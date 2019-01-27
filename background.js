a40b0161ac8974d90e2bc08f2d59c29236a15f57
Kévin GRILLET <kevin@pc53.home>
2019-01-27 12:13:16 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,19 @@
+var updateLogo = function(url, tabId){
+  if (url == undefined) {
+    chrome.browserAction.setIcon({path: 'logo-disabled.png', tabId: tabId});
+  } else if (url.match(/^.*matchhistory\..*\.leagueoflegends.com\/.*/)){
+    chrome.browserAction.setIcon({path: 'logo.png', tabId: tabId});
+  } else {
+    chrome.browserAction.setIcon({path: 'logo-disabled.png', tabId: tabId});
+  }	
+}
+
+chrome.tabs.onActivated.addListener(function(info){
+  chrome.tabs.get(info.tabId, function(change){
+	updateLogo(change.url, info.tabId);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  updateLogo(tab.url, tabId);
+});
